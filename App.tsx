ccdee1835605099bbfb3db33061e8334f000c3e5
Elie-MK <elie.mk25@gmail.com>
2024-09-01 03:41:29 +0100

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,10 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import { loadFonts } from "./config/fonts";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    const fetchFonts = async () => {
+      await loadFonts();
+      setFontsLoaded(true);
+    };
+    fetchFonts();
+  }, []);
+
+  if (fontsLoaded) {
+    SplashScreen.hideAsync();
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={{ fontFamily: "Roboto-Thin" }}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ config/fonts.ts
@@ -0,0 +1,12 @@
+import * as Font from "expo-font";
+
+export const loadFonts = async (): Promise<void> => {
+  await Font.loadAsync({
+    "Roboto-Black": require("../assets/fonts/RobotoBlack.ttf"),
+    "Roboto-Bold": require("../assets/fonts/RobotoBold.ttf"),
+    "Roboto-Light": require("../assets/fonts/RobotoLight.ttf"),
+    "Roboto-Medium": require("../assets/fonts/RobotoMedium.ttf"),
+    "Roboto-Regular": require("../assets/fonts/RobotoRegular.ttf"),
+    "Roboto-Thin": require("../assets/fonts/RobotoThin.ttf"),
+  });
+};
